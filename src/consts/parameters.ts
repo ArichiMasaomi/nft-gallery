10a44550f5ead651172db6acefc38c11d838f4e7
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2024-05-23 16:35:32 +0800

    1

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -1,18 +1,37 @@
 import { createThirdwebClient, getContract } from "thirdweb";
-import { ethereum } from "thirdweb/chains";
+// import { ethereum } from "thirdweb/chains";
+import { defineChain } from "thirdweb/chains";
 
+
+export const redstone = /* @__PURE__ */ defineChain({
+  id: 620,
+  name: "Redstone",
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpc: "https://rpc.redstonechain.com",
+  blockExplorers: [
+    {
+      name: "Etherscan",
+      url: "https://etherscan.io",
+    },
+  ],
+});
 /** Change these values to configure the application for your own use. **/
 
 export const client = createThirdwebClient({
   clientId: import.meta.env.VITE_TEMPLATE_CLIENT_ID,
+  secretKey: import.meta.env.VITE_TEMPLATE_SECRET_KEY,
 });
 
 export const nftContract = getContract({
   // Your smart contract address (available on the thirdweb dashboard)
-  address: "0xed5af388653567af2f388e6224dc7c4b3241c544",
+  address: "0xe680743004614a15db9873aa1aaf0ca3b101619f",
   // The chain object of the chain your contract is deployed to.
   // If that chain isn't in the default list of our SDK, use `defineChain` - for example: defineChain(666666)
-  chain: ethereum,
+  chain: redstone,
   client,
 });
 
